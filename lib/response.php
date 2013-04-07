3f424ca39c341f8a4a39f68209130be98088240a
Andre Bluehs <hello@andrebluehs.net>
2013-04-07 03:26:24 -0500

    200

--- lib/response.php
+++ lib/response.php
@@ -14,7 +14,7 @@ class Response {
     }
     
     static function out($msg = array(), $code = 404){
-        if ($code !== false) {
+        if ($code !== 200) {
             header("HTTP/1.0 $code ". self::$response_codes[$code]);
         }
         echo json_encode($msg);
